182f7be5e023fd8a4a42c27cfe3d10445acc5b0d
Lukas-Batema <lukasbatema@mg.batemadevelopment.com>
2023-01-26 20:04:21 -0500

    Uncaught TypeError: The specifier “@codemirror/state” was a bare specifier, but was not remapped to anything. Relative module specifiers must start with “./”, “../” or “/”. (need to still fix)

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { EditorState } from "@codemirror/state";
+import { keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+import { StreamLanguage } from "@codemirror/language";
+import { csharp } from "@codemirror/legacy-modes/mode/clike";
+import { EditorView, basicSetup } from "codemirror";
+let startState = EditorState.create({
+    doc: 'Console.WriteLine("Hello, World!");',
+    extensions: [
+        keymap.of(defaultKeymap)
+    ]
+});
+let view = new EditorView({
+    state: startState,
+    parent: document.querySelector("#editor"),
+    extensions: [
+        basicSetup,
+        StreamLanguage.define(csharp)
+    ]
+});
+//# sourceMappingURL=index.js.map
