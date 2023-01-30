fc30bcbb326656522c528ba49bcf66e34735c67b
Lukas-Batema <lukasbatema@mg.batemadevelopment.com>
2023-01-30 18:09:27 -0500

    Still working on a fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
   input: "src/bundles/full.bundle.js",
   output: {
+    name: "full",
     file: "src/bundles/full.rollup.bundle.js",
     format: "iife"
   },
--- src/editor.ts
+++ src/editor.ts
@@ -1,10 +1,50 @@
 import { EditorView, basicSetup } from "codemirror";
-import { EditorState } from "@codemirror/state";
-import { keymap } from "@codemirror/view";
+import { EditorState, StateField, StateEffect, Extension } from "@codemirror/state";
+import { keymap, showPanel, Panel } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 import { StreamLanguage } from "@codemirror/language";
 import { csharp } from "@codemirror/legacy-modes/mode/clike";
 import { oneDarkTheme } from "@codemirror/theme-one-dark";
+
+const toggleHelp = StateEffect.define<boolean>();
+
+const helpPanelState = StateField.define<boolean>({
+  create: () => false,
+  update(value, tr) {
+    for (let e of tr.effects) {
+      if (e.is(toggleHelp)) {
+        value = e.value;
+        return value;
+      }
+    }
+  },
+  provide: f => showPanel.from(f, on => on ? createHelpPanel : null)
+});
+
+function createHelpPanel(view: EditorView) {
+  let dom = document.createElement('div');
+  dom.textContent = "F1: Toggle the help panel";
+  dom.className = "cm-help-panel";
+  return { top: true, dom };
+}
+
+const helpKeymap = [{
+  key: "F1",
+  run(view) {
+    view.dispatch({
+      effects: toggleHelp.of(!view.state.field(helpPanelState)),
+    });
+    return true;
+  }
+}];
+
+let console: Extension = {
+  extension: [
+    EditorView.updateListener.of(update => {
+      document.getElementById("output")!.textContent = update.state.doc.toString();
+    })
+  ]
+};
 
 let startState = EditorState.create({
   doc: "using System;\nConsole.WriteLine(\"Hello, World!\");",
@@ -12,11 +52,17 @@ let startState = EditorState.create({
     keymap.of(defaultKeymap),
     basicSetup,
     StreamLanguage.define(csharp),
-    oneDarkTheme
-  ],
+    oneDarkTheme,
+    helpPanel(),
+    console
+  ]
 });
 
-new EditorView({
+;(window as any).view = new EditorView({
   state: startState,
-  parent: document.querySelector("#editor")!
+  parent: document.querySelector("#editor")!,
 });
+
+export function helpPanel() {
+  return [helpPanelState, keymap.of(helpKeymap)];
+}
